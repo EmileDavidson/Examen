be0471bf99d551e5516934a02bacb6792c5dd6ec
Emile // Tundersz <61781276+EmileDavidson@users.noreply.github.com>
2023-03-23 13:36:46 +0100

    removed some duplicates

--- ExamenProeveUnity/Assets/Scripts/Runtime/CharacterController.cs
+++ ExamenProeveUnity/Assets/Scripts/Runtime/CharacterController.cs
@@ -7,32 +7,25 @@ namespace Runtime
         [SerializeField] private float speed = 5f;
         [SerializeField] private ConfigurableJoint hipJoint;
         [SerializeField] private Rigidbody hip;
-
         [SerializeField] private Animator targetAnimator;
 
         private bool _walk;
         private static readonly int Walk = Animator.StringToHash("Walk");
 
-        // Update is called once per frame
-        void Update()
+        private void Update()
         {
             float horizontal = Input.GetAxisRaw("Horizontal");
             float vertical = Input.GetAxisRaw("Vertical");
 
             Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
-
-            if (direction.magnitude >= 0.1f)
+            _walk = (direction.magnitude >= 0.1f);
+        
+            if (_walk)
             {
-                float targetAngle = Mathf.Atan2(direction.z, direction.x) * Mathf.Rad2Deg;
-
+                var targetAngle = Mathf.Atan2(direction.z, direction.x) * Mathf.Rad2Deg;
                 hipJoint.targetRotation = Quaternion.Euler(0f, targetAngle, 0f);
-
                 hip.AddForce(direction * speed);
-
-                _walk = true;
-            }  else {
-                _walk = false;
-            }
+            } 
 
             targetAnimator.SetBool(Walk, _walk);
         }
