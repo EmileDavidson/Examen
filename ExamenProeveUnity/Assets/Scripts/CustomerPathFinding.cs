9f551df7d5b04b81981110e4590ca1b05a8436ce
GodlyHamster <wouter.van.dompselaar@gmail.com>
2023-03-30 14:06:36 +0200

    Added customer pathfinding

--- ExamenProeveUnity/Assets/Scripts/CustomerPathFinding.cs
+++ ExamenProeveUnity/Assets/Scripts/CustomerPathFinding.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,43 +7,60 @@ public class CustomerPathFinding : MonoBehaviour
     [SerializeField] private List<PathNode> _path;
     
     [SerializeField] private Rigidbody hipRb;
+    [SerializeField] private ConfigurableJoint hipJoint;
 
     private Grid<PathNode> _grid;
     private PathFinding _pathFinding;
+
+    private Vector3 _moveDirection;
 
     private void Awake()
     {
         _pathFinding = new PathFinding(10, 10);
         _grid = _pathFinding.Grid;
+        
+        _pathFinding.BlockNode(0, 1, true);
+        _pathFinding.BlockNode(1, 2, true);
+        _pathFinding.BlockNode(2, 2, true);
+        
         InvokeRepeating(nameof(ReCalculatePath), 1f, 1f);
+    }
+
+    private void FixedUpdate()
+    {
+        // hipRb.velocity = Vector3.zero;
+        var targetAngle = Mathf.Atan2(_moveDirection.z, _moveDirection.x) * Mathf.Rad2Deg;
+        float rotationSpeed = (hipJoint.targetRotation.eulerAngles - new Vector3(0f, targetAngle, 0f)).magnitude;
+        hipJoint.targetRotation = Quaternion.RotateTowards(hipJoint.targetRotation, Quaternion.Euler(0f, targetAngle, 0f), rotationSpeed);
+        hipRb.position += _moveDirection / 5f;
     }
 
     public void ReCalculatePath()
     {
         Vector3 playerPos = hipRb.gameObject.transform.position;
-        Vector2 gridPosition = new Vector2(_grid.GetPositionInGrid(playerPos).X, _grid.GetPositionInGrid(playerPos).Y);
-        print(gridPosition);
+        Vector2Int gridPosition = new Vector2Int(_grid.GetPositionInGrid(playerPos).X, _grid.GetPositionInGrid(playerPos).Y);
+
+        _path = _pathFinding.FindPath(gridPosition.x, gridPosition.y, 3, 7);
+        
+        if (_path == null) return;
+        
+        DrawPathDebug(_path);
+
+        if (_path.Count > 1)
+        {
+            PathNode nextNode = _path[1];
+            _moveDirection = (new Vector3(nextNode.X, 0, nextNode.Y) - new Vector3(playerPos.x, 0, playerPos.z)).normalized;
+        }
     }
 
-    private void Start()
+    private void DrawPathDebug(List<PathNode> pathNodes)
     {
-        _pathFinding.BlockNode(0, 1, true);
-        _pathFinding.BlockNode(1, 2, true);
-        _pathFinding.BlockNode(2, 2, true);
-        
-        _path = _pathFinding.FindPath(0, 0, 3, 7);
-        if (_path != null)
+        foreach (PathNode pathNode in pathNodes)
         {
-            foreach (PathNode pathNode in _path)
-            {
-                if (_path.IndexOf(pathNode) + 1 >= _path.Count) return;
-                PathNode nextNode = _path[_path.IndexOf(pathNode) + 1];
+            if (_path.IndexOf(pathNode) + 1 >= pathNodes.Count) return;
+            PathNode nextNode = pathNodes[pathNodes.IndexOf(pathNode) + 1];
                 
-                // Debug.Log("Currently at: " + pathNode.X + ", " + pathNode.Y);
-                // Debug.Log("Next node at: " + nextNode.X + ", " + nextNode.Y);
-                
-                Debug.DrawLine(new Vector3(pathNode.X + 0.5f, 0, pathNode.Y + 0.5f), new Vector3(nextNode.X + 0.5f, 0, nextNode.Y + 0.5f), Color.blue, 100f);
-            }
+            Debug.DrawLine(new Vector3(pathNode.X + 0.5f, 0, pathNode.Y + 0.5f), new Vector3(nextNode.X + 0.5f, 0, nextNode.Y + 0.5f), Color.blue, 1f);
         }
     }
 }
