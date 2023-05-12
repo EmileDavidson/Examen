87a0f0be452f7eb69a9d09a1d0a02043dc0af83c
GodlyHamster <wouter.van.dompselaar@gmail.com>
2023-05-12 15:06:02 +0200

    Truck can now arrive and depart
    Also fixed item display issue with shelves

--- ExamenProeveUnity/Assets/Scripts/Runtime/Products/Truck.cs
+++ ExamenProeveUnity/Assets/Scripts/Runtime/Products/Truck.cs
@@ -1,15 +1,25 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using Toolbox.Attributes;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class Truck : MonoBehaviour
 {
+    [SerializeField] private int speed;
     [SerializeField] private int playersNeeded = 2;
-    
-    public UnityEvent onDoorsOpened;
 
+    [SerializeField] private Vector3 startPos;
+    [SerializeField] private Vector3 endPos;
+    private Vector3 targetPos;
+
+    public UnityEvent onArrive;
+    public UnityEvent onDoorsOpened;
+    public UnityEvent onDepart;
+
+    private bool _atDestination = true;
     private int _playersGrabbing;
 
     private Grabbable _grabbable;
@@ -27,6 +37,36 @@ public class Truck : MonoBehaviour
         if (_playersGrabbing >= playersNeeded)
         {
             onDoorsOpened.Invoke();
+            Depart();
         }
+    }
+
+    [Button]
+    public void Arrive()
+    {
+        onArrive.Invoke();
+        targetPos = endPos;
+        _atDestination = false;
+    }
+
+    [Button]
+    public void Depart()
+    {
+        onDepart.Invoke();
+        targetPos = startPos;
+        _atDestination = false;
+    }
+
+    private void Update()
+    {
+        if (_atDestination) return;
+        if (Vector3.Distance(transform.position, targetPos) <= 1)
+        {
+            _atDestination = true;
+            return;
+        }
+        
+        Vector3 direction = (targetPos - transform.position).normalized;
+        gameObject.transform.position += direction * speed * Time.deltaTime;
     }
 }
