4b8f10686cf0b056f837690927a5b5ff4f223dd7
GodlyHamster <wouter.van.dompselaar@gmail.com>
2023-05-24 13:22:38 +0200

    Extracted methods and improved code

--- ExamenProeveUnity/Assets/Scripts/Runtime/UserInterfaces/PlayerGrabRenderer.cs
+++ ExamenProeveUnity/Assets/Scripts/Runtime/UserInterfaces/PlayerGrabRenderer.cs
@@ -5,6 +5,7 @@ using Runtime.Enums;
 using Runtime.Managers;
 using Runtime.Player;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
 public class PlayerGrabRenderer : MonoBehaviour
@@ -14,30 +15,21 @@ public class PlayerGrabRenderer : MonoBehaviour
     [SerializeField] private PlayerGrab rightHand;
     [SerializeField] private Image leftHandImage;
     [SerializeField] private Image rightHandImage;
-    private List<Entity> _players;
+    private List<PlayerInput> _players;
     private List<PlayerGrab> _playerGrabs;
 
     private void Awake()
     {
-        _players = PlayerManager.Instance.Players;
+        _players = PlayerManager.Instance.PlayerInputs;
         foreach (var player in _players)
         {
-            print(_players.IndexOf(player));
-            if (PlayerManager.Instance.PlayerInputs[_players.IndexOf(player)].playerIndex != playerIndex) continue;
+            if (player.playerIndex != playerIndex) continue;
             _playerGrabs = player.GetComponentsInChildren<PlayerGrab>().ToList();
         }
         
         if (_playerGrabs == null) return;
         
-        foreach (var playerGrab in _playerGrabs)
-        {
-            if (playerGrab.HandType == HandType.Left)
-            {
-                leftHand = playerGrab;
-                continue;
-            }
-            rightHand = playerGrab;
-        }
+        AssignHands();
         
         leftHand.onGrab.AddListener(UpdateVisual);
         leftHand.onRelease.AddListener(UpdateVisual);
@@ -45,6 +37,20 @@ public class PlayerGrabRenderer : MonoBehaviour
         rightHand.onRelease.AddListener(UpdateVisual);
         
         UpdateVisual();
+    }
+
+    private void AssignHands()
+    {
+        foreach (var playerGrab in _playerGrabs)
+        {
+            if (playerGrab.HandType == HandType.Left)
+            {
+                leftHand = playerGrab;
+                continue;
+            }
+
+            rightHand = playerGrab;
+        }
     }
 
     private void UpdateVisual()
